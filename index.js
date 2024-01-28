e729a044d25519ecddb0ff5c13d4d0737db206ba
Team Black Falcon <147306959+teamblackfalcon@users.noreply.github.com>
2024-01-27 19:41:40 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-module.exports = {
-    ddosguard:require('./ddosguard'),
-    blazingfast: require('./blazingfast'),
-    browser_engine: require('./browser_engine'),
-    ovh: require('./ovh'),
-    pipeguard: require('./pipeguard'),
-    privacypass: require('./privacypass'),
-    sucuri: require('./sucuri'),
-    cloudflare: require('./cloudflare'),
-    stormwall: require('./stormwall')
-}
+module.exports = require("./lib/randomstring");
